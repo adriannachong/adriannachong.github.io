3b4d8a6fd3a2aba4e6d2c6c43dfdb16b12158cba
Adrianna <chongadrianna@gmail.com>
2022-10-16 16:26:50 +0800

    remove menu on mobile
    adding js to remove menu view when viewing on mobile

--- js/main.js
+++ js/main.js
@@ -45,3 +45,15 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
+// remove menu when viewing on mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
